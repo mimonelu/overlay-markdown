7c553230c80e6ddbaa68dc0b45cb8a9a3a30409b
mimonelu <mimonelu@gmail.com>
2024-06-20 00:34:00 +0900

    update

---
+++ index.mjs
@@ -0,0 +1,47 @@
+import marked from "marked"
+
+let data = ""
+let inputDisplay = false
+
+function main () {
+  const input = document.body.querySelector(".input")
+  input.addEventListener("input", onInput)
+  const inputToggle = document.body.querySelector(".input-toggle")
+  inputToggle.addEventListener("click", toggleInput)
+  const output = document.body.querySelector(".output")
+  output.addEventListener("mouseenter", onMouseEnterOutput, true)
+  output.addEventListener("mouseleave", onMouseLeaveOutput, true)
+  data = window.localStorage.getItem("data") ?? ""
+  input.value = data
+  update()
+}
+
+function onInput (event) {
+  data = event.target.value
+  window.localStorage.setItem("data", data)
+  update()
+}
+
+function toggleInput () {
+  inputDisplay = !inputDisplay
+  document.body.setAttribute("data-input-display", inputDisplay)
+}
+
+function onMouseEnterOutput (event) {
+  if (event.target.tagName === "EM") {
+    event.target.style.color = "rgb(255, 128, 128)"
+  }
+}
+
+function onMouseLeaveOutput (event) {
+  if (event.target.tagName === "EM") {
+    event.target.style.color = null
+  }
+}
+
+function update () {
+  const output = document.body.querySelector(".output")
+  output.innerHTML = marked.parse(data)
+}
+
+main()
